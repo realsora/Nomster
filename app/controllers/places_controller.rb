f85113e03b43fa15d6ea97c28375729ead5efa39
realsora <realsora@gmail.com>
2019-10-20 03:06:01 +0000

    detail page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,8 +15,14 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place=Place.find(params[:id])
+  end
+
   private
+
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
 end
+ 
