9f3e98efaee440ee2bf0f61e6b804d69e7ba2564
realsora <realsora@gmail.com>
2019-10-21 02:44:23 +0000

    validation added2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,30 +24,24 @@ class PlacesController < ApplicationController
   end
   
   def edit
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-    @place.update_attributes(place_params)
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :edit, status: :unprocessable_entity
-    end
+  @place = Place.find(params[:id])
+if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
   end
+end
 
-  def update
-    @place=Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-    @place.update_attributes(place_params)
-    if @place.valid?
-    redirect_to root_path
-   else
-    render :edit, status: :unprocessable_entity
-   end
+ def update
+  @place = Place.find(params[:id])
+if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
   end
+@place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
 
   def destroy
     @place=Place.find(params[:id])
