4de540ddb896fd668907791d5228ef4d2aa0a0a9
realsora <realsora@gmail.com>
2019-10-28 13:48:28 +0000

    test AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIAYO2CZIL4QT5IA6PL"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["utbootcamp"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
